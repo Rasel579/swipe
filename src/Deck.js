290889a5461628286f4be82322bfb58cfdf1bbcf
Ruslan <rasel579@gmail.com>
2019-03-16 18:04:23 +0500

    add interpolation system for rotating card

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH =Dimensions.get('window').width;
 
 class Deck extends Component{
 
@@ -20,19 +22,41 @@ class Deck extends Component{
     }
 
     renderCards(){
-        return this.props.data.map(item => {
+        return this.props.data.map((item, index) => {
+            if( index === 0 ){
+                return (
+                    <Animated.View
+                        key={item.id}
+                        { ...this.state.panResponder.panHandlers} 
+                        style={this.getCardsStyle()}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                );
+            }
             return this.props.renderCard(item);
         });
     }
 
+    getCardsStyle(){
+
+        const { position } = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH*1.5, 0, SCREEN_WIDTH*1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+
+        return {
+            ...position.getLayout(),
+            transform: [{ rotate }]
+         }
+    }
+
     render(){
         return(
-            <Animated.View 
-            { ...this.state.panResponder.panHandlers} 
-            style={this.state.position.getLayout()}
-            >
+            <View>
                 {this.renderCards()}
-            </Animated.View>
+            </View>
         );
     }
 }
